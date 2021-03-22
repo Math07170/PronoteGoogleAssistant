81d9de0538a82fc5380053d9029666de690439dd
cesium <mathiasr07@hotmail.fr>
2021-03-22 19:07:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const { stringify } = require('actions-on-google/dist/common');
 
 const app = dialogflow();
 
-app.intent('Default Welcome Intent', async conv => {
+app.intent('Default Welcome Intent', async(conv) => {
   if(conv.user.storage.username !== null && conv.user.storage.password !== null ){
     if(conv.user.storage.name === null){
       const session = await pronote.login(url, username, password/*, cas*/);
