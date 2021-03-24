d8a43fa20119624cd3457945b52483689ba98f39
cesium <mathiasr07@hotmail.fr>
2021-03-24 18:47:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -102,7 +102,9 @@ app.intent('Devoirs', async(conv, args)=>{
   reponse = "<speak>Vous avez :"
   const session = await pronote.login(url, conv.user.storage.username, conv.user.storage.password/*, cas*/);
   matiere = args.matieres
-  console.log(args['date-time'])
+  console.log(typeof args['date-time'])
+  console.log(typeof args['date-time'] !== 'undefined')
+  console.log(arg['date-time'] === '')
   if(typeof args['date-time'] !== 'undefined' || arg['date-time'] === ''){
 
     //Un jour
